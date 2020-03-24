8653d3f26912c96f61c224cb6953b5ce221d698b
keith-ratcliffe <ratcliffe.keith@gmail.com>
2020-03-24 18:03:29 -0400

    Fixes #790: Updates to fix query tracing

--- web-services/common/src/main/java/datawave/webservice/common/connection/AccumuloConnectionFactoryBean.java
+++ web-services/common/src/main/java/datawave/webservice/common/connection/AccumuloConnectionFactoryBean.java
@@ -11,20 +11,14 @@ import datawave.webservice.common.result.ConnectionPool;
 import org.apache.accumulo.core.Constants;
 import org.apache.accumulo.core.client.ClientConfiguration;
 import org.apache.accumulo.core.client.Connector;
-import org.apache.accumulo.core.client.ZooKeeperInstance;
 import org.apache.accumulo.core.client.admin.SecurityOperations;
 import org.apache.accumulo.core.client.security.tokens.PasswordToken;
 import org.apache.accumulo.core.trace.DistributedTrace;
 import org.apache.accumulo.core.util.Pair;
-import org.apache.accumulo.core.zookeeper.ZooUtil;
-import org.apache.accumulo.tracer.AsyncSpanReceiver;
-import org.apache.accumulo.tracer.ZooTraceClient;
 import org.apache.commons.lang.mutable.MutableInt;
 import org.apache.deltaspike.core.api.exclude.Exclude;
 import org.apache.deltaspike.core.api.jmx.JmxManaged;
 import org.apache.deltaspike.core.api.jmx.MBean;
-import org.apache.htrace.HTraceConfiguration;
-import org.apache.htrace.Trace;
 import org.apache.log4j.Logger;
 import org.jboss.resteasy.annotations.GZIP;
 
@@ -132,17 +126,17 @@ public class AccumuloConnectionFactoryBean implements AccumuloConnectionFactory 
                 log.warn("Unable to retrieve system property \"app\": " + e.getMessage());
             }
             try {
-                ClientConfiguration zkConfig = ClientConfiguration.loadDefault().withInstance(entry.getValue().getInstance())
+                ClientConfiguration traceConfig = ClientConfiguration.loadDefault().withInstance(entry.getValue().getInstance())
                                 .withZkHosts(entry.getValue().getZookeepers());
-                ZooKeeperInstance instance = new ZooKeeperInstance(zkConfig);
-                Map<String,String> confMap = new HashMap<>();
-                confMap.put(DistributedTrace.TRACER_ZK_HOST, instance.getZooKeepers());
-                confMap.put(DistributedTrace.TRACER_ZK_PATH, ZooUtil.getRoot(instance) + Constants.ZTRACERS);
-                confMap.put(DistributedTrace.TRACE_HOST_PROPERTY, InetAddress.getLocalHost().getHostName());
-                confMap.put(DistributedTrace.TRACE_SERVICE_PROPERTY, appName);
-                confMap.put(AsyncSpanReceiver.SEND_TIMER_MILLIS, "1000");
-                confMap.put(AsyncSpanReceiver.QUEUE_SIZE, "5000");
-                Trace.addReceiver(new ZooTraceClient(HTraceConfiguration.fromMap(confMap)));
+                traceConfig.setProperty(DistributedTrace.TRACER_ZK_PATH, Constants.ZTRACERS);
+                /*
+                 * TODO: Add capability to use Zipkin as an additional sink for DW and Accumulo traces That can be as easy as adding
+                 * org.apache.htrace.impl.ZipkinSpanReceiver to the receivers list here - but requiring that the default Zipkin server location is running on
+                 * localhost:9410. Otherwise, we'll need to be able to configure alternate host, port, etc. Note that htrace-zipkin:3.1.0-incubating is required
+                 * for compatibility with Accumulo 1.9.x tracing.
+                 */
+                traceConfig.setProperty(ClientConfiguration.ClientProperty.TRACE_SPAN_RECEIVERS, "org.apache.accumulo.tracer.ZooTraceClient");
+                DistributedTrace.enable(InetAddress.getLocalHost().getHostName(), appName, traceConfig);
             } catch (IOException e) {
                 log.error("Unable to initialize distributed tracing system: " + e.getMessage(), e);
             }
